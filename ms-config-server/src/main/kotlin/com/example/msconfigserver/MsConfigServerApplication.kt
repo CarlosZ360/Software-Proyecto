757a8d415c1374aa32b4bdd086993caac3a47513
CarlosZ360 <carlos.zarate@ucb.edu.bo>
2023-04-22 22:57:47 -0400

    mvn

---
+++ ms-config-server/src/main/kotlin/com/example/msconfigserver/MsConfigServerApplication.kt
@@ -0,0 +1,16 @@
+package com.example.msconfigserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.config.server.EnableConfigServer
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+class MsConfigServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsConfigServerApplication>(*args)
+}
---
+++ ms-gateway/src/main/kotlin/ucb/arqsoft/msgateway/MsGatewayApplication.kt
@@ -0,0 +1,15 @@
+package ucb.arqsoft.msgateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
+class MsGatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsGatewayApplication>(*args)
+}
