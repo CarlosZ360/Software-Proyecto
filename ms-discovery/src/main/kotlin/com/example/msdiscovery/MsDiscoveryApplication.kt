19cbe7f2cd70e159486996df676e5333db5fc566
CarlosZ360 <carlos.zarate@ucb.edu.bo>
2023-04-23 19:19:45 -0400

    Avance del servidor de configuraciones :)

--- ms-discovery/src/main/kotlin/com/example/msdiscovery/MsDiscoveryApplication.kt
+++ ms-discovery/src/main/kotlin/com/example/msdiscovery/MsDiscoveryApplication.kt
@@ -3,9 +3,11 @@ package com.example.msdiscovery
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
 @EnableEurekaServer
+@Configuration
 class MsDiscoveryApplication
 
 fun main(args: Array<String>) {
--- ms-gateway/src/main/kotlin/ucb/arqsoft/msgateway/MsGatewayApplication.kt
+++ ms-gateway/src/main/kotlin/ucb/arqsoft/msgateway/MsGatewayApplication.kt
@@ -4,10 +4,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableEurekaClient
+@Configuration
 class MsGatewayApplication
 
 fun main(args: Array<String>) {
