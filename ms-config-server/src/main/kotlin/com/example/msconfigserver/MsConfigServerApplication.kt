90bffa5197a0448a8756b19830eaf554044bb2a5
EllieT7 <elizabeth.tacachira@gmail.com>
2023-04-24 12:48:02 -0400

    Servidor de configuraciones funcionando

--- ms-config-server/src/main/kotlin/com/example/msconfigserver/MsConfigServerApplication.kt
+++ ms-config-server/src/main/kotlin/com/example/msconfigserver/MsConfigServerApplication.kt
@@ -2,9 +2,7 @@ package com.example.msconfigserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.config.server.EnableConfigServer
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
 @EnableConfigServer
